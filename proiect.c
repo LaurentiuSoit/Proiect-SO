31a9b3fc5c6529cfffdeaa2de50635701989d812
soitlaur <60842345+soitlaur@users.noreply.github.com>
2023-11-27 18:38:46 +0200

    Saptamana 8 final

--- proiect.c
+++ proiect.c
@@ -7,52 +7,71 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-char* extensie_fisier(char *filename) 
+char *extensie_fisier(char *filename)
 {
     char *dot = strrchr(filename, '.');
-    if(!dot || dot == filename) 
+    if (!dot || dot == filename)
         return "";
     return dot + 1;
 }
 
 void citire_fisier(char *filename, char *output)
 {
-    if(strcmp("statistica.txt", filename) == 0)
+    if (strcmp("statistica.txt", filename) == 0)
         return;
     struct stat st_input;
     int is_bmp = 0;
     int width;
     int height;
 
-    if(stat(filename, &st_input) == -1)
+    if (stat(filename, &st_input) == -1)
     {
         printf("error file\n");
         return;
     }
 
-    if(!S_ISREG(st_input.st_mode))
+    if (!S_ISREG(st_input.st_mode))
     {
         printf("not regular file\n");
         return;
     }
 
-    if(strcmp(extensie_fisier(filename), "bmp") == 0)
+    if (strcmp(extensie_fisier(filename), "bmp") == 0)
     {
         is_bmp = 1;
         int file_descriptor;
-        if((file_descriptor = open(filename, O_RDONLY)) == -1)
+        if ((file_descriptor = open(filename, O_RDWR)) == -1)
         {
-            printf("Nu s-a putut deschide fisierul.\n");
+            printf("Nu s-a putut citi fisierul.\n");
             exit(-1);
         }
 
-
-        lseek(file_descriptor, 16, SEEK_SET);
+        lseek(file_descriptor, 18, SEEK_SET);
         read(file_descriptor, &width, sizeof(int));
         read(file_descriptor, &height, sizeof(int));
 
-        if(close(file_descriptor)==-1)
+        int pixel_size = 3;
+
+        lseek(file_descriptor, 54, SEEK_SET);
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                unsigned char pixel[3];
+                read(file_descriptor, &pixel, sizeof(unsigned char) * pixel_size);
+
+                unsigned char grey = 0.299 * pixel[0] + 0.587 * pixel[1] + 0.114 * pixel[2];
+                pixel[0] = pixel[1] = pixel[2] = grey;
+
+                lseek(file_descriptor, -pixel_size, SEEK_CUR);
+                write(file_descriptor, &pixel, sizeof(unsigned char) * pixel_size);
+            }
+        }
+
+        if (close(file_descriptor) == -1)
         {
             printf("error close\n");
             exit(1);
@@ -75,85 +94,88 @@ void citire_fisier(char *filename, char *output)
     otherAccess[2] = (S_IXOTH == (st_input.st_mode) ? 'x' : '-');
     otherAccess[3] = '\0';
     char finalString[500];
-    if(is_bmp)
+    if (is_bmp)
     {
         sprintf(finalString, "nume fisier : %s\ninaltime : %d\nlatime : %d\ndimensiune : %ld\nidentificatorul utilizatorului : %d\n"
-                "timpul ultimei modificari : %s\ncontorul de legaturi : %ld\ndrepturi de acces user : %s\ndrepturi de acces grup : %s\n"
-                "drepturi de acces altii : %s\n", filename, height, width, st_input.st_size,  st_input.st_uid, ctime(&st_input.st_ctime),
+                             "timpul ultimei modificari : %s\ncontorul de legaturi : %ld\ndrepturi de acces user : %s\ndrepturi de acces grup : %s\n"
+                             "drepturi de acces altii : %s\n",
+                filename, height, width, st_input.st_size, st_input.st_uid, ctime(&st_input.st_ctime),
                 st_input.st_nlink, userAccess, groupAccess, otherAccess);
     }
     else
     {
         sprintf(finalString, "nume fisier : %s\ndimensiune : %ld\nidentificatorul utilizatorului : %d\n"
-                "timpul ultimei modificari : %s\ncontorul de legaturi : %ld\ndrepturi de acces user : %s\ndrepturi de acces grup : %s\n"
-                "drepturi de acces altii : %s\n\n\n", filename, st_input.st_size,  st_input.st_uid, ctime(&st_input.st_ctime),
+                             "timpul ultimei modificari : %s\ncontorul de legaturi : %ld\ndrepturi de acces user : %s\ndrepturi de acces grup : %s\n"
+                             "drepturi de acces altii : %s\n\n\n",
+                filename, st_input.st_size, st_input.st_uid, ctime(&st_input.st_ctime),
                 st_input.st_nlink, userAccess, groupAccess, otherAccess);
     }
-    
+
     int output_descriptor;
-    if((output_descriptor = open(output, O_WRONLY | O_APPEND | O_CREAT)) == -1)
+    if ((output_descriptor = open(output, O_CREAT | O_WRONLY | O_APPEND, S_IWUSR)) == -1)
     {
-        printf("Nu s-a putut deschide fisierul.\n");
+        printf("Nu s-a putut scrie fisierul.\n");
         exit(-1);
     }
 
-    if(write(output_descriptor, finalString, strlen(finalString)) == -1)
+    if (write(output_descriptor, finalString, strlen(finalString)) == -1)
     {
         printf("error write\n");
         exit(1);
     }
 
-    if(close(output_descriptor)==-1)
+    if (close(output_descriptor) == -1)
     {
         printf("error close\n");
         exit(1);
     }
 }
 
-void citire_director(char *director_input, char *director_output){
+void citire_director(char *director_input, char *director_output)
+{
     DIR *dir;
     dir = opendir(director_input);
     struct dirent *entry;
-    struct stat st_file;
-    char str[500];
 
-    while((entry = readdir(dir)) != NULL)
+    while ((entry = readdir(dir)) != NULL)
     {
         printf("%s\n", entry->d_name);
-        char output_file_name[200];
+        char output_file_name[600];
         sprintf(output_file_name, "%s\\%s_statistica.txt", director_output, entry->d_name);
         pid_t pid = fork();
-        if(pid == -1)
+        int status;
+        if (pid == -1)
         {
             printf("Error forking process\n");
             exit(EXIT_FAILURE);
         }
-        else if(pid == 0)
+        else if (pid == 0)
         {
             citire_fisier(entry->d_name, output_file_name);
             exit(EXIT_SUCCESS);
         }
+        while ((pid = wait(&status)) != -1)
         {
-
+            if (WIFEXITED(status))
+            {
+                printf("pid = %d, status = %d\n", pid, WEXITSTATUS(status));
+            }
         }
     }
 }
 
 int main(int argc, char *argv[])
 {
-    if(argc != 2)
+    if (argc != 3)
     {
         printf("Argumente incorecte.\n");
         exit(-1);
-
     }
 
     citire_director(argv[1], argv[2]);
-    
+
     return 0;
-
 }
-
 
 /*
     pid_t pid;
